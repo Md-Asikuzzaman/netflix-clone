0fd6a6a53e4526eb692308ad9583cfba74e499e9
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-08-05 13:29:09 +0600

    fix bugs for deploy

--- lib/prisma.ts
+++
@@ -1,9 +0,0 @@
-import type { PrismaClient as ImportedPrismaClient } from '@prisma/client';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url ?? __filename);
-const { PrismaClient: RequiredPrismaClient } = require('@prisma/client');
-const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient;
-class PrismaClient extends _PrismaClient {}
-
-export const prisma = new PrismaClient();
