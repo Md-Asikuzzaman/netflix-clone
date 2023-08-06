1ec6d6e2804531ff2abb77f14117f94f2e84d637
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-08-06 15:26:33 +0600

    fix bugs for deploy

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,13 +10,13 @@ import { prisma } from './db';
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
 
     GitHubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
     CredentialsProvider({
       id: 'credentials',
